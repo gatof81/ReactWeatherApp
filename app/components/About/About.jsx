8b8acef9e23da970e4d048197c998f25c8f613e2
Diego Ferreyra <diego.ferreyra@toptal.com>
2016-12-30 02:38:35 -0300

    Styling about page

--- app/components/About/About.jsx
+++ app/components/About/About.jsx
@@ -2,7 +2,23 @@ var React = require('react');
 
 var About = props => {
   return(
-    <h3>About component</h3>
+    <div>
+      <h1 className="text-centered">About</h1>
+      <p>
+        This is a wheather application build on React. I build this using The
+        complete React web developer course.
+      </p>
+      <p>
+        <ol>
+          <li>
+            <a href="https://facebook.github.io/react">React</a>
+          </li>
+          <li>
+            <a href="http://openweathermap.org">Open Weather Map</a>
+          </li>
+        </ol>
+      </p>
+    </div>
   );
 }
 module.exports = About;
