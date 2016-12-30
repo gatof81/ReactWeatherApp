7c828bcefe38b0dde178482f206980ca942ffd3b
Diego Ferreyra <diego.ferreyra@toptal.com>
2016-12-30 02:33:09 -0300

    add grid and example styling

--- app/components/Examples/examples.jsx
+++ app/components/Examples/examples.jsx
@@ -1,8 +1,20 @@
 var React = require('react');
+var {Link} = require('react-router')
 
 var Examples = props => {
   return(
-    <h3>Examples</h3>
+    <div>
+      <h1 className="text-centered">Examples</h1>
+      <p>Here are a few examples locations to try out.</p>
+      <ol>
+        <li>
+          <Link to="/?location=Cordoba">Cordoba, AR</Link>
+        </li>
+        <li>
+          <Link to="/?location=Rio">Rio, BR</Link>
+        </li>
+      </ol>
+    </div>
   )
 }
 
